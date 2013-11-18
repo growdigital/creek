24204ec7928550a1d72fae06a9c4db6c5a4f1344
Jake Rayson <jake@growdigital.org>
2013-11-18 15:09:56 +0000

    added styling for home page wireframe

--- header.php
+++ header.php
@@ -1,4 +1,4 @@
-<header class="header_main wire_block wire_nav" role="banner">
+<header class="header wire_block wire_nav" role="banner">
 
 <!--   
   <h1><a href="<?php bloginfo('url'); ?>" title="<?php bloginfo('name'); ?>"><?php bloginfo('name'); ?></a></h1>
@@ -7,5 +7,9 @@
   <nav class="nav" id="menu" role="navigation">
     <?php wp_nav_menu( array( 'theme_location' => 'menu_primary' ) ); ?>
   </nav>
+  <div class="contact">
+    1 Abbey Street, Faversham, <a href="https://maps.google.co.uk/maps?expflags=enable_star_based_justifications:true&ie=UTF8&cid=6632365635905548926&q=Creek+Creative+Studios&iwloc=A&gl=GB&hl=en">ME13 7BE</a><br/>
+     ‎<a href="tel:+441795535515">01795 535 515</a>
+  </div>
 
 </header>
--- index.php
+++ index.php
@@ -21,7 +21,7 @@
 
 <?php while (have_posts()) : the_post(); ?>
   <article <?php post_class(); ?>>
-    <h1><?php the_title(); ?></h1>
+    <h1 class="article__head"><?php the_title(); ?></h1>
     <?php the_content(); ?>
   </article>
 <?php endwhile; ?>
