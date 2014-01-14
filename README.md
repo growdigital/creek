b3000072ba8e5a6bc6de93dc7743ff34b694f87d
Jake Rayson <jake@growdigital.org>
2014-01-14 16:51:24 +0000

    added some instructions, finally managed to get artist names with commas on exhibitions. bleurgh.

--- README.md
+++ README.md
@@ -5,3 +5,27 @@ Creek Creative WordPress website theme.
 
 This work is licensed under the Creative Commons Attribution-ShareAlike 3.0 Unported License.  
 To view a copy of this license, visit <http://creativecommons.org/licenses/by-sa/3.0/deed.en_US>
+
+Instructions for using GruntJS
+------------------------------
+
+### Installation
+
+* Install the grunt command-line tool (-g puts it in /usr/local/bin):  
+`% sudo npm install -g grunt-cli`
+* Install all the packages required to build this:  
+(Packages will be installed in ./node_modules - don't accidentally commit this)  
+`% cd wp-content/themes/theme_name`  
+`% npm install`
+
+### Building
+
+    % grunt
+
+Watch for changes:
+
+    % grunt watch
+
+If you create new `.scss` blocks in `_blocks` directory, restart `% grunt watch`
+
+**NB:** make sure to be using the latest version of [node](http://nodejs.org/). I had a `Bus error: 10` error, solved by [upgrading node](http://stackoverflow.com/questions/20521685/grunt-task-exits-with-bus-error-10).
--- front-page.php
+++ front-page.php
@@ -1,38 +1,199 @@
 <?php get_header('includes/header.php'); ?>
 
-<?php while (have_posts()) : the_post(); ?>
+<?php // while (have_posts()) : the_post(); ?>
   <article <?php post_class(); ?>>
     <h1 class="article__head"><?php the_title(); ?></h1>
     <?php the_content(); ?>
   </article>
-<?php endwhile; ?>
+<?php // endwhile; ?>
 
-<hr/>
+
+<section class="exhibitions">
+
+<h1>Exhibitions</h1>
+
+<div class="exhibitions-current">
+
+<h2>Current </h2>
+
+
+
+<?php // Using ACF repeater normal array
+// $rows = get_field('carousel');
+// if($rows)
+//   { 
+//     echo '<ol class="carousel-indicators">';
+//     foreach($rows as $i => $row) {
+//       echo '<li class="';
+//       if ($i == 0) echo 'active'; 
+//       echo '" data-target="#carousel" data-slide-to="' . $i++ .'"></li>';
+//     }
+//     echo '</ol><!-- / carousel-indicators -->';
+//     echo '<div class="carousel-inner">';
+// 
+//     foreach($rows as $i => $row) {
+//       echo '<div class="item';
+//       if ($i == 0) echo ' active'; 
+//       echo '"><img src="'. $row['carousel_image']['url'] .'" alt="' . $row['carousel_image_desc'] . '" /></div>';
+//     }
+//     echo '</div><!-- / .carousel-inner -->';
+//   }
+// ?>
 
 <?php
-  // Exhibitions
-  $exhibitions = eo_get_events(array(
-    'numberposts'=>all,
+
+// If you use get_posts() or WP_Query instead then you should ensure the following:
+// 
+// post_type - is set to 'event'
+// suppress_filters - is set to false
+
+?>
+
+<h3 style="color:red;">all post_type event</h3>
+<?php 
+  $args = array(
+    'numberposts'      => all,
+    'post_type'        => 'event',
+    'post_status'      => 'publish',
+    'suppress_filters' => false 
+  );
+  $events = get_posts($args);
+  foreach ($events as $post) : setup_postdata($post); 
+?>
+<li>
+  <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a><br/>
+  <?php $thumb = wp_get_attachment_image_src(get_field('poster_image'), 'thumbnail'); ?>
+  <a href="<?php the_permalink(); ?>"><img src="<?php echo $thumb[0]; ?>" alt="" /></a>
+  <?php 
+    $artists = get_field('artist');
+    if($artists) {
+      echo '<p>';
+      foreach($artists as $artist) {
+        $artistName[] = $artist['artist_name'];
+      }
+      echo implode(', ', $artistName);
+      unset($artistName);
+      echo '</p>';
+    }
+  ?>
+ 
+<?php 
+  endforeach; 
+  wp_reset_postdata();
+?>
+
+<hr>
+<h3 style="color:red;">Event Organiser custom taxonomy <code>event-category</code></h3>
+
+<? 
+  // Get EO categories http://wordpress.stackexchange.com/questions/58734/how-to-use-get-categories-with-event-organiser-plugin
+  $taxonomies = array('event-category');
+  $terms = get_terms($taxonomies, $args);
+  var_dump($terms); 
+?>
+<hr/>
+<h3 style="color:red;">EO example for current exhibition</h3>
+<?php
+  // Exhibitions current
+  $exhibitions_current = eo_get_events(array(
+   'numberposts'=>all,
+    'tax_query'=>array(array(
+      'taxonomy'=>'event-category',
+      'operator'=>'IN',
+      'field'=>'slug',
+      'terms'=>array('exhibition')
+      )),
     'event_start_before'=>'today',
-    'showpastevents'=>true, //Will be deprecated, but set it to true to play it safe.
+    'event_end_after'=>'today',
+    'showpastevents'=>true // Will be deprecated, but set it to true to play it safe.
   ));
 
-  if($exhibitions):
-    echo '<ul>';
-    foreach ($exhibitions as $event):
-      //Check if all day, set format accordingly
+  if($exhibitions_current):
+    foreach ($exhibitions_current as $event):
+      // Check if all day, set format accordingly
       $format = ( eo_is_all_day($event->ID) ? get_option('date_format') : get_option('date_format').' '.get_option('time_format') );
       printf(
-        '<li><a href="%s">%s</a> on %s</li>',
+        '<article><a href="%s">%s</a> on %s</article>',
         get_permalink($event->ID),
         get_the_title($event->ID),
         eo_get_the_start($format, $event->ID,null,$event->occurrence_id)
       );
     endforeach;
-    echo '</ul>';
+  endif;
+
+ ?>
+</div>
+
+
+<div class="exhibitions-future">
+
+<h2>Next</h2>
+<?php
+  // Exhibitions next
+  $exhibitions_next = eo_get_events(array(
+    'numberposts'=>all,
+    'tax_query'=>array(array(
+      'taxonomy'=>'event-category',
+      'operator'=>'IN',
+      'field'=>'slug',
+      'terms'=>array('exhibition')
+      )),
+    'event_start_after'=>'today',
+    'showpastevents'=>true //Will be deprecated, but set it to true to play it safe.
+  ));
+  if($exhibitions_next):
+    foreach ($exhibitions_next as $event):
+      //Check if all day, set format accordingly
+      $format = ( eo_is_all_day($event->ID) ? get_option('date_format') : get_option('date_format').' '.get_option('time_format') );
+      printf(
+        '<article><a href="%s">%s</a> on %s</article>',
+        get_permalink($event->ID),
+        get_the_title($event->ID),
+        eo_get_the_start($format, $event->ID,null,$event->occurrence_id)
+      );
+    endforeach;
   endif;
  ?>
+
+</div><!-- /.exhibitions-future -->
+
+<div class="exhibitions-past">
+
+<h2>Past Exhibitions</h2>
+
+<?php
+  // Exhibitions past
+  $exhibitions_past = eo_get_events(array(
+    'numberposts'=>4,
+    'tax_query'=>array(array(
+      'taxonomy'=>'event-category',
+      'operator'=>'IN',
+      'field'=>'slug',
+      'terms'=>array('exhibition')
+      )),
+    'event_start_before'=>'today',
+    'showpastevents'=>true //Will be deprecated, but set it to true to play it safe.
+  ));
+  if($exhibitions_past):
+    foreach ($exhibitions_past as $event):
+      //Check if all day, set format accordingly
+      $format = ( eo_is_all_day($event->ID) ? get_option('date_format') : get_option('date_format').' '.get_option('time_format') );
+      printf(
+        '<article><a href="%s">%s</a> on %s</article>',
+        get_permalink($event->ID),
+        get_the_title($event->ID),
+        eo_get_the_start($format, $event->ID,null,$event->occurrence_id)
+      );
+    endforeach;
+  endif;
+ ?>
+
 <hr/>
+</div><!-- /.exhibitions-past -->
+
+
+</section><!-- /.exhibitions -->
+
 
 
 <section><!-- home exhibitions -->
