7b2abc13ddd2d5bb23d0da73e6b734cacbbf0b25
Jake Rayson <jake@growdigital.org>
2014-03-17 19:54:08 +0000

    Refining gulpfile, added svg to png, tidied paths

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,8 @@ var gulp = require('gulp'),
   minifyCSS = require('gulp-minify-css'),
   pixrem = require('gulp-pixrem'),
   svgmin = require('gulp-svgmin'),
+  svg2png = require('gulp-svg2png'),
+
 
   // Server
   tinyLr = require('tiny-lr'),
@@ -19,80 +21,90 @@ var gulp = require('gulp'),
 
   // Define paths
   paths = {
-      css: [
-        'src/assets/styles/alpha.css',
-        'src/assets/styles/variables.css',
-        'src/bower_components/normalize/normalize.css',
-        'src/assets/styles/base.css',
-        'src/bower_components/suit-utils-*/*.css',
-        'src/patternlab/**/**/*.css',
-        'src/patternlab/**/**/**/*.css',
-        'src/assets/styles/shame.css'
-      ],
-      js: [
-        'src/assets/scripts/*.js',
-        'src/patternlab/**/**/*.js',
-        'src/patternlab/**/**/**/*.js'
-      ],
-      jquery: [
-        'src/bower_components/jquery/dist/jquery.min.js',
-      ],
-      svg: [
-        'src/assets/images/svg/*.svg'
-      ],
-      bitmap: [
-        'src/assets/images/svg/*.svg'
-      ]
+    css: [
+      'src/assets/css/alpha.css',
+      'src/assets/css/variables.css',
+      'src/bower_components/normalize/normalize.css',
+      'src/assets/css/base.css',
+      'src/bower_components/suit-utils-*/*.css',
+      'src/patternlab/**/**/*.css',
+      'src/patternlab/**/**/**/*.css',
+      'src/assets/css/shame.css'
+    ],
+    js: [
+      'src/assets/js/*.js',
+      'src/patternlab/**/**/*.js',
+      'src/patternlab/**/**/**/*.js'
+    ],
+    jquery: [
+      'src/bower_components/jquery/dist/jquery.min.js'
+    ],
+    svg: [
+      'src/assets/img/svg/*.svg'
+    ],
+    bitmap: [
+      'src/assets/img/bitmap/24bit/*'
+    ],
+    transbit: [
+      'tmp/assets/img/trans/*'
+    ]
   };
 
 // Error function
 function handleError(type, e) {
-    gutil.log(gutil.colors.red('Error for task: "' + type));
-    gutil.log(gutil.colors.red(e.message));
+  gutil.log(gutil.colors.red('Error for task: "' + type));
+  gutil.log(gutil.colors.red(e.message));
 }
 
 // CSS task
 gulp.task('css', function() {
-    var stream = gulp.src(paths.css)
-        .pipe(myth())
-        .on('error', function(e){ handleError('Run Myth processing',e);})
-        .pipe(concat("styles.css"))
-        .on('error', function(e){ handleError('Concat CSS files',e);})
-        .pipe(pixrem())
-        .on('error', function(e){ handleError('Pixel fallback for rems',e);})
-        .pipe(minifyCSS())
-        .on('error', function(e){ handleError('Minify CSS files',e);})
-        .pipe(gulp.dest('static'))
-        .on('error', function(e){ handleError('Save CSS file',e);});
+  var stream = gulp.src(paths.css)
+    .pipe(myth())
+    .on('error', function(e){ handleError('Run Myth processing',e);})
+    .pipe(concat("styles.css"))
+    .on('error', function(e){ handleError('Concat CSS files',e);})
+    .pipe(pixrem())
+    .on('error', function(e){ handleError('Pixel fallback for rems',e);})
+    .pipe(minifyCSS())
+    .on('error', function(e){ handleError('Minify CSS files',e);})
+    .pipe(gulp.dest('dist/assets/css'))
+    .on('error', function(e){ handleError('Save CSS file',e);});
 });
 
-// SVG task
-gulp.task('svg', function() {
-    return gulp.src(paths.svg)
-        .pipe(svgmin())
-        .pipe(gulp.dest('static/assets/images'));
+// SVG optimisation task
+gulp.task('svgo', function() {
+  return gulp.src(paths.svg)
+    .pipe(svgmin())
+    .pipe(gulp.dest('dist/assets/img'));
+});
+
+// SVG to PNG task
+gulp.task('svg2png', function() {
+  return gulp.src(paths.svg)
+    .pipe(svg2png())
+    .pipe(gulp.dest('tmp/assets/img/trans'));
 });
 
 // Server tasks
 gulp.task('livereload', ['tiny-lr-server'], function() {
-    gulp.src(['static/styles.css','static/assets/images/*.svg'])
-        .pipe(watch())
-        .on('error', function(e){ handleError('Watch file for LiveReload refresh',e);})
-        .pipe(livereload(liveReloadServer));
+  gulp.src(['dist/assets/css/styles.css','dist/assets/img/*.svg'])
+    .pipe(watch())
+    .on('error', function(e){ handleError('Watch file for LiveReload refresh',e);})
+    .pipe(livereload(liveReloadServer));
 });
 
 gulp.task('tiny-lr-server', function(next) {
-    liveReloadServer.listen(35729, function(err) {
-        if (err) return console.error(err);
-        next();
-    });
+  liveReloadServer.listen(35729, function(err) {
+    if (err) return console.error(err);
+    next();
+  });
 });
 
 // Watch task
 gulp.task('watch', function () {
-    gulp.watch(paths.css, ['css'])
-    gulp.watch(paths.svg, ['svg']);
+  gulp.watch(paths.css, ['css'])
+  gulp.watch(paths.svg, ['svgo']);
 });
 
 // Default task
-gulp.task('default', ['css', 'svg', 'watch', 'livereload']);
+gulp.task('default', ['css', 'svgo', 'svg2png', 'watch', 'livereload']);
