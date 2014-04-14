c27baee4c39c1486e5446b5f3cc9e1524b174b9f
Jake Rayson <jake@growdigital.org>
2014-04-14 12:42:04 +0100

    Styled Section

--- front-page.php
+++ front-page.php
@@ -4,191 +4,182 @@
 
 <section class="exhibitions">
 
-<h1 class="u-isHiddenVisually">Exhibitions</h1>
+  <h1 class="u-isHiddenVisually">Exhibitions</h1>
 
-<!-- CURRENT EXHIBITIONS -->
-<div class="Section u-cf exhibitions-current">
-<h2 class="Heading Heading--support">Current exhibitions</h2>
-<?php
-  $args = array(
-    'numberposts'        => 'all',
-    'post_type'          => 'event',
-    'event-category'     => 'exhibition',
-    'post_status'        => 'publish',
-    'event_start_before' => 'today',
-    'event_end_after'    => 'today',
-    'suppress_filters'   =>  false
-  );
-  $events_current = get_posts($args);
-?>
-<?php foreach ($events_current as $post) : setup_postdata($post); ?>
-<article class="ArticleThumb Media Media--golden">
-  <?php $thumb = wp_get_attachment_image_src(get_field('poster_image'), 'a4_thumbnail'); ?>
-  <a class="Media-img Media-img--golden" href="<?php the_permalink(); ?>"><img class="ArticleThumb-img" src="<?php echo $thumb[0]; ?>" alt="" /></a>
-  <div class="Media-body Media-body--golden">
-    <h1 class="Heading Heading--less"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+  <!-- CURRENT EXHIBITIONS -->
+  <div>
+    <h2 class="Heading Heading--support">Current exhibitions</h2>
     <?php
-      $artists = get_field('artist');
-      if($artists) {
-        echo '<p>';
-        foreach($artists as $artist) {
-          $artistName[] = $artist['artist_name'];
-        }
-        echo implode(', ', $artistName);
-        unset($artistName);
-        echo '</p>';
-      }
+      $args = array(
+        'numberposts'        => 'all',
+        'post_type'          => 'event',
+        'event-category'     => 'exhibition',
+        'post_status'        => 'publish',
+        'event_start_before' => 'today',
+        'event_end_after'    => 'today',
+        'suppress_filters'   =>  false
+      );
+      $events_current = get_posts($args);
     ?>
-    <p class="ArticleThumb-desc"><?php $excerpt =  get_field('event_excerpt'); echo $excerpt; ?></p>
-    <p class="ArticleThumb-date"><?php echo eo_get_the_start('D jS M Y'); ?> <br class="u-RespBreak">to <?php echo eo_get_the_end('D jS M Y'); ?></p>
+    <?php foreach ($events_current as $post) : setup_postdata($post); ?>
+    <article class="ArticleThumb Media Media--golden">
+      <?php $thumb = wp_get_attachment_image_src(get_field('poster_image'), 'a4_thumbnail'); ?>
+      <a class="Media-img Media-img--golden" href="<?php the_permalink(); ?>"><img class="ArticleThumb-img" src="<?php echo $thumb[0]; ?>" alt="" /></a>
+      <div class="Media-body Media-body--golden">
+        <h1 class="Heading Heading--less"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+        <?php
+          $artists = get_field('artist');
+          if($artists) {
+            echo '<p>';
+            foreach($artists as $artist) {
+              $artistName[] = $artist['artist_name'];
+            }
+            echo implode(', ', $artistName);
+            unset($artistName);
+            echo '</p>';
+          }
+        ?>
+        <p class="ArticleThumb-desc"><?php $excerpt =  get_field('event_excerpt'); echo $excerpt; ?></p>
+        <p class="ArticleThumb-date"><?php echo eo_get_the_start('D jS M Y'); ?> <br class="u-RespBreak">to <?php echo eo_get_the_end('D jS M Y'); ?></p>
+      </div>
+    </article>
+    <?php
+      endforeach;
+      wp_reset_postdata();
+    ?>
   </div>
-</article>
-<?php
-  endforeach;
-  wp_reset_postdata();
-?>
 
-</div><!-- /.exhibitions-current -->
+  <!-- NEXT EXHIBITIONS -->
+  <div>
+    <h2 class="Heading Heading--support">Next exhibitions</h2>
 
-<!-- NEXT EXHIBITIONS -->
-<div class="exhibitions-next">
-<h2 class="Heading Heading--support">Next exhibitions</h2>
-
-<?php
-  $args = array(
-    'numberposts'       => 'all',
-    'post_type'         => 'event',
-    'event-category'    => 'exhibition',
-    'post_status'       => 'publish',
-    'event_start_after' => 'today',
-    'suppress_filters'  =>  false
-  );
-  $events_next = get_posts($args);
-  echo '<ul class="Listing">';
-?>
-<?php foreach ($events_next as $post) : setup_postdata($post); ?>
-<li>
-  <h3 class="Heading Heading--less"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-  <p class="ArticleThumb-desc"><?php $excerpt =  get_field('event_excerpt'); echo $excerpt; ?></p>
-  <p class="ArticleThumb-date"><?php echo eo_get_the_start('D jS M Y'); ?> to <?php echo eo_get_the_end('D jS M Y'); ?></p>
-</li>
-<?php
-  endforeach;
-  wp_reset_postdata();
-  echo '</ul>';
-?>
-
-</div><!-- /.exhibitions-next -->
-</section><!-- /.exhibitions -->
+    <?php
+      $args = array(
+        'numberposts'       => 'all',
+        'post_type'         => 'event',
+        'event-category'    => 'exhibition',
+        'post_status'       => 'publish',
+        'event_start_after' => 'today',
+        'suppress_filters'  =>  false
+      );
+      $events_next = get_posts($args);
+      echo '<ul class="Listing">';
+    ?>
+    <?php foreach ($events_next as $post) : setup_postdata($post); ?>
+    <li>
+      <h3 class="Heading Heading--less"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+      <p class="ArticleThumb-desc"><?php $excerpt =  get_field('event_excerpt'); echo $excerpt; ?></p>
+      <p class="ArticleThumb-date"><?php echo eo_get_the_start('D jS M Y'); ?> to <?php echo eo_get_the_end('D jS M Y'); ?></p>
+    </li>
+    <?php
+      endforeach;
+      wp_reset_postdata();
+      echo '</ul>';
+    ?>
+  </div>
+</section>
 
 <section class="courses">
 
-<!-- *** NEXT COURSES *** -->
-<div class="courses_coming_up">
-<h2 class="Heading Heading--support">Courses coming up</h2>
+  <!-- NEXT COURSES -->
+  <div class="courses_coming_up">
+  <h2 class="Heading Heading--support">Upcoming courses</h2>
 
-<?php
-  $args = array(
-    'numberposts'        => 'all',
-    'post_type'          => 'event',
-    'event-category'     => 'course',
-    'group_events_by'    => 'series',
-    'post_status'        => 'publish',
-    'event_start_after'  => 'today',
-    'order'              => 'ASC',
-    'suppress_filters'   =>  false
-  );
-  $courses_next = get_posts($args);
-?>
-<?php foreach ($courses_next as $post) : setup_postdata($post); ?>
-<article class="ArticleThumb Media Media--golden">
-  <?php $thumb = wp_get_attachment_image_src(get_field('poster_image'), 'a4_thumbnail'); ?>
-  <a class="Media-img Media-img--golden" href="<?php the_permalink(); ?>"><img class="ArticleThumb-img" src="<?php echo $thumb[0]; ?>" alt="" /></a>
-  <div class="Media-body Media-body--golden">
-    <h1 class="Heading Heading--less"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-    <p class="ArticleThumb-desc"><?php $excerpt =  get_field('event_excerpt'); echo $excerpt; ?></p>
-    <p class="ArticleThumb-date">
-      <?php if( eo_reoccurs() ){
-                  echo eo_get_schedule_start('D jS M Y') . ' to ' . eo_get_schedule_end('D jS M Y');
-            }else{
-                  echo eo_get_the_start('D jS M Y');
-            }
-       ?>
-      </p>
+  <?php
+    $args = array(
+      'numberposts'        => 'all',
+      'post_type'          => 'event',
+      'event-category'     => 'course',
+      'group_events_by'    => 'series',
+      'post_status'        => 'publish',
+      'event_start_after'  => 'today',
+      'order'              => 'ASC',
+      'suppress_filters'   =>  false
+    );
+    $courses_next = get_posts($args);
+  ?>
+  <?php foreach ($courses_next as $post) : setup_postdata($post); ?>
+  <article class="ArticleThumb Media Media--golden">
+    <?php $thumb = wp_get_attachment_image_src(get_field('poster_image'), 'a4_thumbnail'); ?>
+    <a class="Media-img Media-img--golden" href="<?php the_permalink(); ?>"><img class="ArticleThumb-img" src="<?php echo $thumb[0]; ?>" alt="" /></a>
+    <div class="Media-body Media-body--golden">
+      <h1 class="Heading Heading--less"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+      <p class="ArticleThumb-desc"><?php $excerpt =  get_field('event_excerpt'); echo $excerpt; ?></p>
+      <p class="ArticleThumb-date">
+        <?php if( eo_reoccurs() ){
+                    echo eo_get_schedule_start('D jS M Y') . ' to ' . eo_get_schedule_end('D jS M Y');
+              }else{
+                    echo eo_get_the_start('D jS M Y');
+              }
+         ?>
+        </p>
+    </div>
+  </article>
+  <?php
+    endforeach;
+    wp_reset_postdata();
+  ?>
+
   </div>
-</article>
-<?php
-  endforeach;
-  wp_reset_postdata();
-?>
 
-</div><!-- /.courses_coming_up -->
+  <!-- CURRENT COURSES -->
+  <div class="courses_currently_running">
+  <h2 class="Heading Heading--support">Current courses</h2>
 
-<!-- CURRENT COURSES -->
-<div class="courses_currently_running">
-<h2 class="Heading Heading--support">Courses currently running</h2>
+  <?php
+    $args = array(
+      'numberposts'        => 'all',
+      'post_type'          => 'event',
+      'event-category'     => 'course',
+      'post_status'        => 'publish',
+      'group_events_by'    => 'series',
+      'event_start_before' => 'today',
+      'event_end_after'    => 'today',
+      'suppress_filters'   =>  false
+    );
+    $courses_current = get_posts($args);
+    echo '<ul class="Listing">';
+  ?>
+  <?php foreach ($courses_current as $post) : setup_postdata($post); ?>
+  <li>
+    <h3 class="Heading Heading--less"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+  </li>
+  <?php
+    endforeach;
+    wp_reset_postdata();
+    echo '</ul>';
+  ?>
 
-<?php
-  $args = array(
-    'numberposts'        => 'all',
-    'post_type'          => 'event',
-    'event-category'     => 'course',
-    'post_status'        => 'publish',
-    'group_events_by'    => 'series',
-    'event_start_before' => 'today',
-    'event_end_after'    => 'today',
-    'suppress_filters'   =>  false
-  );
-  $courses_current = get_posts($args);
-  echo '<ul>';
-?>
-<?php foreach ($courses_current as $post) : setup_postdata($post); ?>
-<li>
-  <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-</li>
-<?php
-  endforeach;
-  wp_reset_postdata();
-  echo '</ul>';
-?>
-
-<!-- if no courses, then placeholder? or nothing? -->
-
-</div><!-- /.courses_currently_running -->
-
-</section><!-- /.courses -->
-
-
-<!-- Home page blurb content -->
-
-<?php while (have_posts()) : the_post(); ?>
-  <?php the_content(); ?>
-<?php endwhile; ?>
+  </div>
+</section>
 
 
 <? // $post_type = get_post_type_object('event'); var_dump($post_type);?>
 
-<section class="sections">
-<?php if(get_field('section')): ?>
+<section class="Section-wrap">
 
-  <ul>
+  <h2 class="Heading Heading--support">What we offer</h2>
 
-  <?php while(has_sub_field('section')): ?>
-    <article class="Article u-cf" class="<?php the_sub_field('section_promoted'); ?>"> <!-- TODO: if true, then class = is-promoted -->
-      <h1><?php the_sub_field('section_title'); ?></h1>
-      <img src="<?php
-        $image = get_sub_field('section_image');
-        $size = $image['sizes'];
-        echo $size['medium'] . '"' . ' alt="' . $image['alt'];  ?>"/>
-      <? // var_dump($image); ?>
-    <?php the_sub_field('section_copy'); ?>
-    </article>
-  <?php endwhile; ?>
+  <?php if(get_field('section')): ?>
 
-  </ul>
+    <ul class="Section-list">
 
-<?php endif; ?>
-</section><!-- /.sections -->
+    <?php while(has_sub_field('section')): ?>
+      <article class="Section" class="<?php the_sub_field('section_promoted'); ?>"> <!-- TODO: if true, then class = is-promoted -->
+        <h1 class="Heading Heading--less"><?php the_sub_field('section_title'); ?></h1>
+        <img class="Section-image u-nbfc" src="<?php
+          $image = get_sub_field('section_image');
+          $size = $image['sizes'];
+          echo $size['medium'] . '"' . ' alt="' . $image['alt'];  ?>"/>
+        <? // var_dump($image); ?>
+        <div class="u-nbfc"><?php the_sub_field('section_copy'); ?></div>
+      </article>
+    <?php endwhile; ?>
+
+    </ul>
+
+  <?php endif; ?>
+</section>
 
 </main>
 
