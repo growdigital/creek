4834769d54a4645c2ba01e7d8deaccc336b9a915
Jake Rayson <jake@growdigital.org>
2014-09-25 22:11:32 +0100

    custom post type latest on front page

--- front-page.php
+++ front-page.php
@@ -2,7 +2,16 @@
 
 <main role="main">
 
-
+<?php
+$args = array( 'post_type' => 'latest', 'posts_per_page' => 1 );
+$loop = new WP_Query( $args );
+while ( $loop->have_posts() ) : $loop->the_post();
+  the_title();
+  echo '<div class="entry-content">';
+  the_content();
+  echo '</div>';
+endwhile;
+?>
 
 <section class="Letterbox-wrap">
 
